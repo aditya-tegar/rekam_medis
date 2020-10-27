b3bd730612c44e9912137da922fef5a637e42b31
Esa Rizki <esa.rizki.hari.utama@altomatik.com>
2020-10-27 20:56:12 +0700

    edit mig

--- database/migrations/2020_10_26_134104_create_pasien_table.php
+++ database/migrations/2020_10_26_134104_create_pasien_table.php
@@ -19,12 +19,12 @@ class CreatePasienTable extends Migration
             $table->string('nama_pasien', 30);
             $table->string('tempat', 50);
             $table->date('tanggal_lahir'); 
-            $table->string('alamat', 12);
+            $table->string('alamat', 50);
             $table->string('no_telepon', 30);
             $table->string('status_perkawinan', 30);
-            $table->string('agama', 5);
+            $table->string('agama', 15);
             $table->string('kewarganegaraan', 30);
-            $table->string('diagnosa', 200);
+            $table->string('diagnosa', 200)->nullable();
             $table->timestamps();
         });
     }
